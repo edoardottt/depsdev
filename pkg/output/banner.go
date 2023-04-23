12632d23ddeca7d375bfacf417c2f7d11d1f7ed0
edoardottt <edoardott@gmail.com>
2023-04-23 09:15:49 +0200

    v0.0.1

--- pkg/output/banner.go
+++ pkg/output/banner.go
@@ -17,7 +17,7 @@ package output
 import "fmt"
 
 const (
-	Version = `0.0.1-dev`
+	Version = `0.0.1`
 	Banner  = `depsdev v` + Version + `
   > https://github.com/edoardottt/depsdev
   > @edoardottt, https://www.edoardoottavianelli.it/`
