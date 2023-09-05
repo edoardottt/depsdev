91355564dcf4c796e1c625d86d502423713bdd67
Christophe de Carvalho <christophe@archipelo.co>
2023-09-05 14:54:51 +0100

    complete Version type to includes all fields returned by the API

--- pkg/depsdev/version.go
+++ pkg/depsdev/version.go
@@ -14,12 +14,16 @@ Free access to dependencies, licenses, advisories, and other critical health and
 
 package depsdev
 
+import "time"
+
 type Version struct {
-	VersionKey   VersionKey     `json:"versionKey,omitempty"`
-	IsDefault    bool           `json:"isDefault,omitempty"`
-	Licenses     []string       `json:"licenses,omitempty"`
-	AdvisoryKeys []AdvisoryKeys `json:"advisoryKeys,omitempty"`
-	Links        []Links        `json:"links,omitempty"`
+	VersionKey      VersionKey      `json:"versionKey,omitempty"`
+	IsDefault       bool            `json:"isDefault,omitempty"`
+	Licenses        []string        `json:"licenses,omitempty"`
+	AdvisoryKeys    []AdvisoryKeys  `json:"advisoryKeys,omitempty"`
+	Links           []Links         `json:"links,omitempty"`
+	SlsaProvenances []SLAProvenance `json:"slsaProvenances,omitempty"`
+	PublishedAt     time.Time       `json:"publishedAt,omitempty"`
 }
 
 type AdvisoryKeys struct {
@@ -29,4 +33,10 @@ type AdvisoryKeys struct {
 type Links struct {
 	Label string `json:"label,omitempty"`
 	URL   string `json:"url,omitempty"`
+}
+
+type SLAProvenance struct {
+	SourceRepository string `json:"sourceRepository,omitempty"`
+	Commit           string `json:"commit,omitempty"`
+	URL              string `json:"url,omitempty"`
 }
