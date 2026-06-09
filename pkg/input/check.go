72f23a772aa8fc79099e2e8652a51f910b619aca
edoardottt <edoardott@gmail.com>
2026-06-09 09:05:56 +0200

    linting

--- pkg/input/check.go
+++ pkg/input/check.go
@@ -16,6 +16,7 @@ package input
 
 import "strings"
 
+//nolint:goconst
 var (
 	AllValidPackageManagers  = []string{"go", "rubygems", "npm", "cargo", "maven", "pypi", "nuget"}
 	DepsValidPackageManagers = []string{"npm", "cargo", "maven", "pypi"}
